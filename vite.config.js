44be5179a3d5badf59cc8a829899dfb9b31738da
vinoddhaware <vinoddhaware>
2025-03-15 13:07:29 +0530

    fixed all errors

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/expanse-tracker'
+  base: '/expanse-tracker/'
 })
